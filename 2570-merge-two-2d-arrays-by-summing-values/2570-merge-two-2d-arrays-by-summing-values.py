41ccd264f4ed1ab20b9b965717b0c8d3f51a2fc4
Taqi Javed <37430435+Taqi12@users.noreply.github.com>
2025-12-30 18:02:05 +0500

    Time: 4 ms (9.85%), Space: 17.3 MB (98.22%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.py
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.py
@@ -1,23 +1,59 @@
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-                
-        dict = {}
+        # Approach 1  
+
+        # dict = {}
+        # result = []
+
+        # for i in nums1:
+        #     if i[0] not in dict:
+        #         dict[i[0]] = i[1]
+        #     else:
+        #         dict[i[0]] += i[1]
+
+        # for i in nums2:
+        #     if i[0] not in dict:
+        #         dict[i[0]] = i[1]
+        #     else:
+        #         dict[i[0]] += i[1]
+
+        # # Sort by keys before appending to the result list
+        # for key in sorted(dict.keys()):
+        #     result.append([key, dict[key]])
+
+        # return result
+
+        # Approach 2
+
+        i = j = 0
         result = []
 
-        for i in nums1:
-            if i[0] not in dict:
-                dict[i[0]] = i[1]
-            else:
-                dict[i[0]] += i[1]
+        while i < len(nums1) and j < len(nums2):
 
-        for i in nums2:
-            if i[0] not in dict:
-                dict[i[0]] = i[1]
-            else:
-                dict[i[0]] += i[1]
+            id1 = nums1[i][0]
+            id2 = nums2[j][0]
 
-        # Sort by keys before appending to the result list
-        for key in sorted(dict.keys()):
-            result.append([key, dict[key]])
+            if id1 == id2:
+                result.append([id1, nums1[i][1] + nums2[j][1]])
+                i += 1
+                j += 1
+
+            elif id1 < id2:
+                result.append(nums1[i])
+                i += 1
+
+            else:
+                result.append(nums2[j])
+                j += 1
+
+        # Remaining elements
+        while i < len(nums1):
+            result.append(nums1[i])
+            i += 1
+
+        while j < len(nums2):
+            result.append(nums2[j])
+            j += 1
 
         return result
+
