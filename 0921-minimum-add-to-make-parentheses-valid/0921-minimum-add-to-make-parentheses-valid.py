0fc4b835c939ffcb65fbe81eac1dbb36c5bc8eef
Taqi Javed <37430435+Taqi12@users.noreply.github.com>
2026-01-05 14:46:58 +0500

    Time: 0 ms (100%), Space: 17.4 MB (82.03%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -1,18 +1,35 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
+        # Approach 1
+
+        # stack = []
+        # count = 0
+
+        # for i in s:
+        #     if i == "(":
+        #         stack.append(i)
+        #     else:
+        #         if stack:
+        #             stack.pop()
+        #         else:
+        #             count += 1
         
-        stack = []
-        count = 0
+        # count += len(stack)
+
+        # return count
+
+        # Approach 2
+        
+        add_count = 0
+        remove_count = 0
 
         for i in s:
             if i == "(":
-                stack.append(i)
+                add_count += 1
             else:
-                if stack:
-                    stack.pop()
+                if add_count > 0:
+                    add_count -= 1
                 else:
-                    count += 1
-        
-        count += len(stack)
+                    remove_count += 1
 
-        return count
+        return add_count + remove_count
